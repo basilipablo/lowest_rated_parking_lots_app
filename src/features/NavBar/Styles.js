80147cc8c2b3ccbb80ff1a3b1dea682dd08b5839
Pablo <basilipablo@gmail.com>
2021-07-19 18:37:04 -0300

    created the app, erased some extras, more erase is needed and build the navbar.

---
+++ src/features/NavBar/Styles.js
@@ -0,0 +1,27 @@
+import {
+    makeStyles
+} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    title: {
+        position: "absolute;right:0;top:0",
+        marginTop: theme.spacing(3),
+        marginRight: theme.spacing(20),
+        [theme.breakpoints.down('sm')]: {
+            margin: theme.spacing(1),
+        },
+        color: 'inherit',
+        '&:hover': {
+            cursor: "pointer",
+        }
+    },
+    img: {
+        width: 200,
+        marginLeft: theme.spacing(20),
+    }
+}));
+
+export default useStyles;
